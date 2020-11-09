7fa61545fdcc761f074086a275e1619f723c687a
yamauchitomoya <42059799+plus-agc-service-yamauchi@users.noreply.github.com>
2020-11-09 13:26:23 +0900

    20201109
    update

---
+++ dist/js/sidebar.js
@@ -0,0 +1,13 @@
+/*!
+ * Sidebar for Bootstrap 4
+ * Copyright Zdeněk Papučík
+ * MIT License
+ */
+(function($) {
+
+  // toggle sidebar
+  $('#sidebar').on('click', function() {
+    $('#navigation').toggleClass('sidebar-toggle');
+  });
+
+}(jQuery));
